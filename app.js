acc93a2ea69dcdd4c95c28b32cf2145923cad902
Monirul Islam <Monirul Islam>
2022-11-15 22:59:46 +0600

    home page responsive with header footer

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-var swiper = new Swiper('.mySwiper', {
-  navigation: {
-    nextEl: '.custom-button-next',
-    prevEl: '.custom-button-prev',
-  },
-});
-var swiper2 = new Swiper('#packages', {
-  slidesPerView: 3,
-  spaceBetween: 10,
-  loop: true,
-  navigation: {
-    nextEl: '#packageNext',
-    prevEl: '#packagePrev',
-  },
-});
-
+{
+  const swiper = new Swiper('.mySwiper', {
+    navigation: {
+      nextEl: '.custom-button-next',
+      prevEl: '.custom-button-prev',
+    },
+  });
+  const swiper2 = new Swiper('#packages', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+      nextEl: '#packageNext',
+      prevEl: '#packagePrev',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      520: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      960: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+}
 // header menu toggle
 {
   const menuIconElm = document.querySelector('#menuIcon');
