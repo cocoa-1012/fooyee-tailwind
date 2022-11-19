5447f4a46221cbad14bad1b0d64bebb1dc490ad8
Monirul Islam <Monirul Islam>
2022-11-20 00:34:01 +0600

    block done and web component accordian , alert and badges design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          red: '#FF0000',
+          yellow: '#FFAB07',
+          green: '#0EAD00',
+          black: '#313131',
+        },
+        secondary: {
+          green: {
+            100: '#E2E6DA',
+            200: '#A3B18A',
+            300: '#7A8960',
+          },
+          yellow: {
+            100: '#F3EDD5',
+            200: '#FFCC6A',
+            300: '#FFAB07',
+          },
+          red: {
+            100: '#FFE5E5',
+            200: '#FF7C7C',
+            300: '#FF0000',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
